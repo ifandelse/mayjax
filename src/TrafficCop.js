56d8a2e97cf46de57bc994c217c5ed84dfbafbe4
Jim Cowart <ashbylane@gmail.com>
2011-11-23 01:40:10 -0500

    Initial Add.  Tests to be added next

---
+++ src/TrafficCop.js
@@ -0,0 +1,30 @@
+
+var inProgress = {};
+
+$.trafficCop = function(reqOptions) {
+    var key = JSON.stringify(reqOptions);
+    if(!inProgress[key]) {
+        var remove = function() {
+                delete inProgress[key];
+            },
+            traffic = {
+                successCallbacks: [reqOptions.success],
+                errorCallbacks: [reqOptions.error],
+                success: function(response) {
+                    $.each($(inProgress[key].successCallbacks), function(idx,item){ item(response); });
+                    remove();
+                },
+                error: function(exception) {
+                    $.each($(inProgress[key].errorCallbacks), function(idx,item){ item(exception); });
+                    remove();
+                }
+            };
+        inProgress[key] = $.extend({}, reqOptions, traffic);
+        $.ajax(inProgress[key]);
+    }
+    else {
+        inProgress[key].successCallbacks.push(reqOptions.success);
+        inProgress[key].errorCallbacks.push(reqOptions.error);
+    }
+};
+
