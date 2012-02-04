fd4cbc305f2a110fadc61ceaae211191c7935ad1
Jim Cowart <jim@ifandelse.com>
2012-02-04 01:04:04 -0500

    Added AMD build configuration, updated output libs, added version and license notice.

---
+++ src/VersionHeader.js
@@ -0,0 +1,6 @@
+/*
+    TrafficCop
+    Author: Jim Cowart
+    License: Dual licensed MIT (http://www.opensource.org/licenses/mit-license) & GPL (http://www.opensource.org/licenses/gpl-license)
+    Version 0.3.0
+*/
